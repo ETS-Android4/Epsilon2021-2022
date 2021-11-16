cd9050ea6f5079a554a02f10634a2144ea197829
jthanoan <jon.s.jayaputra@gmail.com>
2021-11-16 13:09:40 -0500

    field centric code test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/FieldCentricTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/FieldCentricTeleOp.java
@@ -16,7 +16,17 @@ public class FieldCentricTeleOp extends LinearOpMode {
         robot.initialize(this);
 
         while (opModeIsActive()){
+            robot.imu.update();
+            double angle = robot.imu.angle;
 
+            double y = -gamepad1.left_stick_y * Math.cos(angle);
+            double x = gamepad1.left_stick_x * Math.sin(angle);
+            double r = gamepad1.right_stick_x;
+
+            robot.drivetrain.frontLeft.setPower(y+r+x);
+            robot.drivetrain.frontRight.setPower(y-r-x);
+            robot.drivetrain.backLeft.setPower(y+r-x);
+            robot.drivetrain.backRight.setPower(y-r+x);
         }
     }
 }
