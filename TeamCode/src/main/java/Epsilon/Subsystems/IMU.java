135e7521ee4adc823632984a0fac5a40c43df830
jthanoan <jon.s.jayaputra@gmail.com>
2021-11-16 09:52:45 -0500

    copied expos code

--- TeamCode/src/main/java/Epsilon/Subsystems/IMU.java
+++ TeamCode/src/main/java/Epsilon/Subsystems/IMU.java
@@ -1,5 +1,7 @@
 package Epsilon.Subsystems;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -9,8 +11,41 @@ import Epsilon.Superclasses.Subsystem;
 
 public class IMU implements Subsystem {
 
-    public void initialize(LinearOpMode opMode) {
+    public BNO055IMU imu;
+    public double angle;
+    private double lastIMUReading;
 
+    public void initialize(LinearOpMode opMode) {
+        imu = opMode.hardwareMap.get(BNO055IMU.class, "imu");
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "AdafruitIMUCalibration.json";
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+        imu.initialize(parameters);
+        //while IMU is not calibrated
+        while (opMode.opModeIsActive() && !imu.isGyroCalibrated());
+        lastIMUReading = imu.getAngularOrientation().firstAngle;
+    }
+
+    public static double normalize(double angle) {
+        // returns the angle geometrically equivalent to the input angle but between -180 and 180
+        angle = angle % 360;
+        if (angle > 180) {
+            angle -= 360;
+        }
+        if (angle < -180) {
+            angle += 360;
+        }
+        return angle;
+    }
+
+    public void update() {
+        double newAngle = imu.getAngularOrientation().firstAngle;
+        angle += normalize(newAngle - lastIMUReading);
+        lastIMUReading = newAngle;
     }
 
 }
