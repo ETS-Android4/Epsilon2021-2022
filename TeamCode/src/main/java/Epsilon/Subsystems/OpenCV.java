d71207a5e9a98784ac2e68d82820b9d814b284df
Jacob Zhang <Jzhang5850@gmail.com>
2021-11-30 17:00:57 -0500

    OpenCV Pipeline Coded

--- TeamCode/src/main/java/Epsilon/Subsystems/OpenCV.java
+++ TeamCode/src/main/java/Epsilon/Subsystems/OpenCV.java
@@ -2,10 +2,19 @@ package Epsilon.Subsystems;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvInternalCamera;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+import java.nio.channels.Pipe;
 
 import Epsilon.Superclasses.Subsystem;
 
@@ -35,5 +44,78 @@ public class OpenCV implements Subsystem {
             }
         });
     }
+    static class Pipeline extends OpenCvPipeline {
+        //https://github.com/OpenFTC/EasyOpenCV/blob/master/examples/src/main/java/org/firstinspires/ftc/teamcode/SkystoneDeterminationExample.java
+        enum ObjectPos {
+            LEFT,
+            CENTER,
+            RIGHT
+        }
+        final Scalar BLUE = new Scalar(0, 0, 255);
+        final Scalar GREEN = new Scalar(0, 255, 0);
 
+        final Point REGION1_TOPLEFT_ANCHOR_POINT = new Point(109,98);
+        final Point REGION2_TOPLEFT_ANCHOR_POINT = new Point(181,98);
+        final Point REGION3_TOPLEFT_ANCHOR_POINT = new Point(253,98);
+        final int REGION_WIDTH = 20;
+        final int REGION_HEIGHT = 20;
+        Point region1_pointA = new Point(
+                REGION1_TOPLEFT_ANCHOR_POINT.x,
+                REGION1_TOPLEFT_ANCHOR_POINT.y);
+        Point region1_pointB = new Point(
+                REGION1_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
+                REGION1_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
+        Point region2_pointA = new Point(
+                REGION2_TOPLEFT_ANCHOR_POINT.x,
+                REGION2_TOPLEFT_ANCHOR_POINT.y);
+        Point region2_pointB = new Point(
+                REGION2_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
+                REGION2_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
+        Point region3_pointA = new Point(
+                REGION3_TOPLEFT_ANCHOR_POINT.x,
+                REGION3_TOPLEFT_ANCHOR_POINT.y);
+        Point region3_pointB = new Point(
+                REGION3_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
+                REGION3_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
+        Mat region1_Cb, region2_Cb, region3_Cb;
+        Mat YCrCb = new Mat();
+        Mat Cb = new Mat();
+        int avg1, avg2, avg3;
+        private ObjectPos position = ObjectPos.LEFT;
+
+        void inputToCb(Mat input)
+        {
+            Imgproc.cvtColor(input, YCrCb, Imgproc.COLOR_RGB2YCrCb);
+            Core.extractChannel(YCrCb, Cb, 2);
+        }
+
+        @Override
+        public void init(Mat firstFrame)
+        {
+            inputToCb(firstFrame);
+            region1_Cb = Cb.submat(new Rect(region1_pointA, region1_pointB));
+            region2_Cb = Cb.submat(new Rect(region2_pointA, region2_pointB));
+            region3_Cb = Cb.submat(new Rect(region3_pointA, region3_pointB));
+        }
+
+        @Override
+        public Mat processFrame(Mat input) {
+            avg1 = (int) Core.mean(region1_Cb).val[0];
+            avg2 = (int) Core.mean(region2_Cb).val[0];
+            avg3 = (int) Core.mean(region3_Cb).val[0];
+            int maxOneTwo = Math.max(avg1, avg2);
+            int max = Math.max(maxOneTwo, avg3);
+            if(max == avg1){
+                position = ObjectPos.LEFT;
+            } else if (max == avg2) {
+                position = ObjectPos.CENTER;
+            } else if (max == avg3) {
+                position = ObjectPos.RIGHT;
+            }
+            return input;
+        }
+        public ObjectPos getAnalysis() {
+            return position;
+        }
+    }
 }
